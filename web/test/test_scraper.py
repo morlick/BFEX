523ac9a8015b973d8ecc40a01744e6b7af8fd0a6
Maggie <morlick123@gmail.com>
2018-02-25 15:50:42 -0700

    Added scraper tests

---
+++ web/test/test_scraper.py
@@ -0,0 +1,21 @@
+import pytest
+from bfex.components.scraper.scraper import Scraper
+from bfex.components.scraper.scraper_type import ScraperType
+
+base_url = "https://www.ualberta.ca/science/about-us/contact-us/faculty-directory/"
+
+
+class TestScraper():
+    def test_create__success(self):
+        my_scraper = Scraper(base_url + "william-allison", ScraperType.PROFILE)
+        assert my_scraper is not None
+        assert my_scraper.url == base_url + "william-allison"
+        assert my_scraper.type == ScraperType.PROFILE
+
+        my_scraper.validate_url()
+        
+        soup = my_scraper.get_content()
+        assert soup is not None
+        
+        scrapps = my_scraper.get_scrapps()
+        assert scrapps == []
