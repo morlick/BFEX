34a461f33384971363ad5a8f60a43e98154b80cb
Lane Scobie <LaneScobie@Lanes-MacBook-Pro.local>
2018-02-26 14:10:25 -0700

    fixed tests for generic approach

--- web/test/test_generic_approach.py
+++ web/test/test_generic_approach.py
@@ -6,10 +6,10 @@ from bfex.components.scraper.scrapp import *
 class TestGenericApproach(object):
     def test_create_success(self):
         type_of_approach = 1
-        my_generic_approach = GenericApproach(type_of_approach,'generic')
+        my_generic_approach = GenericApproach()
         scrapp = Scrapp()
         scrapp.text = "The scraper is responsible from gathering useful information from our webpage data sources. It's goal is to extract any useful text, a title, dates and other meta-data (such as easily identifiable key words)from the page. It will then return that as a 'Scrapp'. A scrapp is a container for the results of a single page scraping"
-        result = my_generic_approach.generate_keywords(scrapp)
+        result = my_generic_approach.generate_keywords(scrapp.text)
         assert result is not None
 
         my_type = my_generic_approach.get_id()
--- web/test/test_key_generator.py
+++ web/test/test_key_generator.py
@@ -11,9 +11,9 @@ class TestKeyGenerator(object):
         scrapp = Scrapp()
         scrapp.text = "The scraper is responsible from gathering useful information from our webpage data sources. It's goal is to extract any useful text, a title, dates and other meta-data (such as easily identifiable key words)from the page. It will then return that as a 'Scrapp'. A scrapp is a container for the results of a single page scraping"
         
-        my_generic_approach = GenericApproach(type_of_approach,'test')
+        my_generic_approach = GenericApproach()
         my_key_generator.register_approach(my_generic_approach, type_of_approach)
-        result = my_key_generator.generate_keywords(scrapp)
+        result = my_key_generator.generate_keywords(scrapp.text)
         assert result is not None
 
         my_key_generator.deregister_approach(type_of_approach)
