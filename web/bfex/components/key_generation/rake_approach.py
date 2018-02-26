fc36c7a23ac36a77c9af826920bb99a9f3b88852
Mandy Meindersma <meinders@ualberta.ca>
2018-02-26 14:41:18 -0700

    Last minute tests

--- web/bfex/components/key_generation/rake_approach.py
+++ web/bfex/components/key_generation/rake_approach.py
@@ -7,18 +7,14 @@ import io
 class RakeApproach(KeyGenerationApproach):
     def __init__(self):
         self.description = """ Generate keyword with RAKE, extract keywords base on finding multi-word phrases containing frequent words, and filter it with stop words """
-        self.apporach_id = 2
+        self.approach_id = 2
     
 
 
     def generate_keywords(self, text):
-        
         rake_object  = rake.Rake(5,3,1)
-            
         keywords_with_score = rake_object.run(text)
-
         keywords = []
-
         for n in range(len(keywords_with_score)):
             keywords.append(keywords_with_score[n][0])
 
