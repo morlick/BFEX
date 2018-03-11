e2904efb51d83cd67ea18f26077e6c9f8e7be9d2
Maggie <morlick123@gmail.com>
2018-03-10 18:02:00 -0700

    ResearchId task and document data model

---
+++ web/bfex/components/data_pipeline/tasks/researchid.py
@@ -0,0 +1,92 @@
+from bfex.components.scraper.scraper_factory import ScraperFactory
+from bfex.components.scraper.scraper_type import ScraperType
+from bfex.models import Faculty, Document
+from bfex.common.utils import URLs, FacultyNames
+from bfex.common.exceptions import WorkflowException
+from bfex.components.data_pipeline.tasks.task import Task
+from bfex.components.key_generation.rake_approach import *
+
+
+class ResearchIdPageScrape(Task):
+    def __init__(self):
+        super().__init__("ResearchId Page Scrape")
+
+    def is_requirement_satisfied(self, data):
+        """ Checks the requirements for a faculty page scraping are satisfied.
+
+        For a ResearchId page scrape, we get the links from ElasticSearch.
+        :param list data: list of all faculty
+        :return: True if valid data, otherwise false
+        """
+        satisfied = True
+
+        for faculty in data:
+
+            if (not isinstance(faculty, tuple) or
+                    not isinstance(faculty[0], str) or
+                    not isinstance(faculty[1], Scrapp)):
+                satisfied = False
+
+        return satisfied
+
+    def run(self, data):
+
+        """Performs a scraping of a faculty members ResearchId page.
+        :param data is a faculty object
+        :return: last faculty member handled
+        """
+        
+        no_text_count = 0
+        for faculty in data:
+            faculty_name = faculty.name
+    
+            search_results = Faculty.search().query('match', name=faculty_name).execute()
+            if len(search_results) > 1:
+                # Shouldn't happen, but could.
+                raise WorkflowException("Professor id is ambiguous during search ... More than 1 result")
+        
+            faculty = search_results[0]
+            if faculty.research_id is not None:
+                
+                scraper = ScraperFactory.create_scraper(faculty.research_id, ScraperType.RESEARCHID)
+                scrapps = scraper.get_scrapps()
+
+                freebies = scrapps[0]
+                titles = scrapps[1:]
+
+                doc = Document()
+                doc.faculty_id = faculty.faculty_id
+                doc.source = "ResearchId"
+                try:
+                    doc.text = freebies.meta_data["description"]
+                except:
+                    print("No description")
+                    doc.text = ""
+                try:
+                    doc.user_keywords = freebies.meta_data["keywords"]
+                except:
+                    print("No keywords")
+                doc.save()
+
+                for scrapp in titles:
+                    doc = Document()
+                    doc.source = "ResearchId"
+                    doc.faculty_id = faculty.faculty_id
+                    doc.text = scrapp.title
+                    doc.save()
+
+            else:
+                no_text_count += 1
+        print("NO TEXT COUNT = ", no_text_count)
+        return faculty
+
+
+if __name__ == "__main__":
+    from elasticsearch_dsl import connections
+    connections.create_connection()
+    Faculty.init()
+
+    search = Faculty.search()
+    allFaculty = [faculty for faculty in search.scan()]
+    task = ResearchIdPageScrape()
+    task.run(allFaculty)
