079ebfd448965e0c1cb94bdfb2e54649ecace9f9
Dennis Truong <dtruong1@ualberta.ca>
2018-03-15 10:47:21 -0600

    Fixed Tests

--- web/test/test_key_generator.py
+++ web/test/test_key_generator.py
@@ -4,11 +4,13 @@ from bfex.components.key_generation.key_generator import *
 from bfex.components.key_generation.generic_approach import *
 from bfex.components.key_generation.rake_approach import *
 from bfex.components.scraper.scrapp import *
+from bfex.common.utils import ConfigFile
 
 class TestKeyGenerator(object):
     def test_create_success(self):
         type_of_approach = 0
         my_key_generator = KeyGenerator.instance()
+        my_key_generator.initialize_keygen()
         print(my_key_generator.approaches)
         scrapp = Scrapp()
         scrapp.text = "The scraper is responsible from gathering useful information from our webpage data sources. It's goal is to extract any useful text, a title, dates and other meta-data (such as easily identifiable key words)from the page. It will then return that as a 'Scrapp'. A scrapp is a container for the results of a single page scraping"
@@ -18,11 +20,16 @@ class TestKeyGenerator(object):
         result = my_key_generator.generate_keywords(scrapp.text)
         assert result is not None
 
-        my_key_generator.deregister_approach(type_of_approach)
+        for id in ConfigFile().data['keygen_ids']:
+            my_key_generator.deregister_approach(id)
         assert my_key_generator.approaches == {}
+
     def test_empty_approach_list(self):
         type_of_approach = 0
-        my_key_generator = KeyGenerator()
+        my_key_generator = KeyGenerator.instance()
+        my_key_generator.initialize_keygen()
+        for id in ConfigFile().data['keygen_ids']:
+            my_key_generator.deregister_approach(id)
         scrapp = Scrapp()
         scrapp.text = "The scraper is responsible from gathering useful information from our webpage data sources. It's goal is to extract any useful text, a title, dates and other meta-data (such as easily identifiable key words)from the page. It will then return that as a 'Scrapp'. A scrapp is a container for the results of a single page scraping"
         
@@ -32,7 +39,8 @@ class TestKeyGenerator(object):
 
     def test_negative_id(self):
         type_of_approach = -1
-        my_key_generator = KeyGenerator()
+        my_key_generator = KeyGenerator.instance()
+        my_key_generator.initialize_keygen()
         scrapp = Scrapp()
         scrapp.text = "The scraper is responsible from gathering useful information from our webpage data sources. It's goal is to extract any useful text, a title, dates and other meta-data (such as easily identifiable key words)from the page. It will then return that as a 'Scrapp'. A scrapp is a container for the results of a single page scraping"
         
@@ -43,7 +51,10 @@ class TestKeyGenerator(object):
 
     def test_register_overwrite(self):
         type_of_approach = 0
-        my_key_generator = KeyGenerator()
+        my_key_generator = KeyGenerator.instance()
+        my_key_generator.initialize_keygen()
+        for id in ConfigFile().data['keygen_ids']:
+            my_key_generator.deregister_approach(id)
         scrapp = Scrapp()
         scrapp.text = "The scraper is responsible from gathering useful information from our webpage data sources. It's goal is to extract any useful text, a title, dates and other meta-data (such as easily identifiable key words)from the page. It will then return that as a 'Scrapp'. A scrapp is a container for the results of a single page scraping"
         
@@ -59,10 +70,14 @@ class TestKeyGenerator(object):
 
     def test_empty_deregister(self):
         type_of_approach = 0
-        my_key_generator = KeyGenerator()
+        my_key_generator = KeyGenerator.instance()
+        my_key_generator.initialize_keygen()
+        for id in ConfigFile().data['keygen_ids']:
+            my_key_generator.deregister_approach(id)
         scrapp = Scrapp()
         scrapp.text = "The scraper is responsible from gathering useful information from our webpage data sources. It's goal is to extract any useful text, a title, dates and other meta-data (such as easily identifiable key words)from the page. It will then return that as a 'Scrapp'. A scrapp is a container for the results of a single page scraping"
-        
+        length_approach = len(my_key_generator.approaches)
+        print(my_key_generator.approaches)
         my_key_generator.deregister_approach(type_of_approach)
         result = my_key_generator.generate_keywords(scrapp.text)
         assert len(result) == 0
